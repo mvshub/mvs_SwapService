2ff456fa294c60e24600c4e59cfc9e9e8088c1db
luozhaohui <zhaohui.luo@viewfin.com>
2018-07-26 18:42:37 +0800

    update scanbusiness

--- services/scanbusiness.py
+++ services/scanbusiness.py
@@ -16,57 +16,59 @@ import time
 import logging
 from decimal import Decimal
 from functools import partial
-import modles.process 
+import modles.process
 
 
 class ScanBusiness(IBusiness):
 
     def __init__(self, service_, rpcmanager_, settings):
-        IBusiness.__init__(self,service=service_, rpc=None, setting=settings)
-        self.rpcs ={}
-        self.rpc_tokens={}
+        IBusiness.__init__(self, service=service_, rpc=None, setting=settings)
+        self.rpcs = {}
+        self.min_confirm_map = {}
         self.rpcmanager = rpcmanager_
- 
+
         for s in self.setting['services']:
             if s['enable']:
-                self.rpcs[s['coin']] = self.rpcmanager.get_available_feed(s['rpc'])
-                self.rpc_tokens[s['coin']]=s['minconf']
+                coin = s['coin']
+                self.rpcs[coin] = self.rpcmanager.get_available_feed(s['rpc'])
+                self.min_confirm_map[coin] = s['minconf']
 
-        self.swicher ={
-        'ETH':'ETP',
-        'ETHToken':'ETP',
-        'ETP':'ETH'
+        self.swicher = {
+            'ETH': 'ETP',
+            'ETHToken': 'ETP',
+            'ETP': 'ETH'
         }
 
-
     @timeit
-    def process_scan(self): 
-        results = db.session.query(Result).filter_by(is_confirm=process.PROCESS_UNCONFIRM).all()
+    def process_scan(self):
+        results = db.session.query(Result).filter_by(
+            is_confirm=process.PROCESS_UNCONFIRM).all()
         if not results:
             return True
-        
-        for r in results:         
+
+        for r in results:
             if r.tx_hash == None or r.coin not in self.swicher:
                 continue
 
-            rpc_name=self.swicher[r.coin]
+            rpc_name = self.swicher[r.coin]
             if rpc_name == 'ETH' and r.token != 'ERC.ETH':
                 rpc_name = 'ETHToken'
 
-            if rpc_name not in self.rpc_tokens:
+            if rpc_name not in self.min_confirm_map:
                 continue
 
             rpc = self.rpcs[rpc_name]
             block_num = rpc.best_block_number()
-            minconf = self.rpc_tokens[rpc_name]      
+            minconf = self.min_confirm_map[rpc_name]
             tx = rpc.get_transaction(r.tx_hash)
 
-            if tx != None and tx['blockNumber']+minconf <= block_num:
+            if tx != None and tx['blockNumber'] + minconf <= block_num:
                 r.is_confirm = process.PROCESS_CONFIRM
-                logging.info('confirm tx:%s,tx_height:%d, cur_number:%d' % (r.tx_hash, tx['blockNumber'],block_num) )
+                logging.info('confirm tx:%s,tx_height:%d, cur_number:%d' %
+                             (r.tx_hash, tx['blockNumber'], block_num))
                 db.session.add(r)
-        
-        db.session.commit()   
+
+        db.session.commit()
 
         return True
 
