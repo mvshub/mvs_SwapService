157f475d0397491534d1d5cc293a09066f5fb87d
luozhaohui <zhaohui.luo@viewfin.com>
2018-08-03 17:42:47 +0800

    update service

--- services/service.py
+++ services/service.py
@@ -7,7 +7,7 @@ from models import constants
 from models.constants import Status, Error, SwapException
 from utils import response
 from utils.log.logger import Logger
-from flask import Flask, jsonify,redirect,url_for
+from flask import Flask, jsonify, redirect, url_for
 import sqlalchemy_utils
 from gevent.pywsgi import WSGIServer
 from gevent import monkey
@@ -19,7 +19,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, FileField, DateTimeField, BooleanField, HiddenField, SubmitField, PasswordField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, Required, Length, Email, Regexp, EqualTo
 from sqlalchemy.sql import func
-from sqlalchemy import or_,and_,case
+from sqlalchemy import or_, and_, case
 import json
 
 
@@ -57,19 +57,19 @@ class MainService(IService):
             return response.make_response(response.ERR_SERVER_ERROR, '404: SwapService page not found')
 
         @self.app.route('/<coin>/<token>/<date>/<int:status>')
-        def swap_status(coin,token,date,status=0):
+        def swap_status(coin, token, date, status=0):
 
             if status == 0:
                 results = db.session.query(Result).filter_by(
-                    date=date,coin=coin,token=token).all()
+                    date=date, coin=coin, token=token).all()
             elif status == 1:
-                results = db.session.query(Result).filter_by(\
-                date=date,coin=coin,token=token,status=int(Status.Swap_Finish)).all()
+                results = db.session.query(Result).filter_by(
+                    date=date, coin=coin, token=token, status=int(Status.Swap_Finish)).all()
             else:
                 results = db.session.query(Result).\
-                filter(and_(Result.date==date, Result.coin==coin, Result.token==token,\
-                Result.status != int(Status.Swap_Finish))).all()
-            
+                    filter(and_(Result.date == date, Result.coin == coin, Result.token == token,
+                                Result.status != int(Status.Swap_Finish))).all()
+
             records = []
             for r in results:
                 record = {}
@@ -79,8 +79,9 @@ class MainService(IService):
                 record['tx_from'] = r.tx_from
                 record['from'] = r.from_address
                 record['to'] = r.to_address
-                record['amount'] = r.amount
-                record['time'] = "%d:%d:%d" % (r.time//10000, r.time//100%100, r.time%100)
+                record['amount'] = self.format_amount(r.amount)
+                record['time'] = "%d:%d:%d" % (
+                    r.time // 10000, r.time // 100 % 100, r.time % 100)
                 record['tx_height'] = r.tx_height
                 record['message'] = constants.ProcessStr(
                     r.status, r.confirm_status)
@@ -90,11 +91,10 @@ class MainService(IService):
 
             return render_template('date.html', date=date, results=records)
 
-
         @self.app.route('/date/<date>')
         def swap_date(date):
             results = db.session.query(Result).filter_by(
-                    date=date).all()
+                date=date).all()
             records = []
             for r in results:
                 record = {}
@@ -104,8 +104,9 @@ class MainService(IService):
                 record['tx_from'] = r.tx_from
                 record['from'] = r.from_address
                 record['to'] = r.to_address
-                record['amount'] = r.amount
-                record['time'] = "%d:%d:%d" % (r.time//10000, r.time//100%100, r.time%100)
+                record['amount'] = self.format_amount(r.amount)
+                record['time'] = "%d:%d:%d" % (
+                    r.time // 10000, r.time // 100 % 100, r.time % 100)
                 record['tx_height'] = r.tx_height
                 record['message'] = constants.ProcessStr(
                     r.status, r.confirm_status)
@@ -118,30 +119,38 @@ class MainService(IService):
         @self.app.route('/<token>')
         def swap_token(token):
             results = db.session.query(Result).filter_by(
-                 token=token).all()
+                token=token).all()
 
             for result in results:
-                result.time = "%d:%d:%d" % (result.time//10000, result.time//100%100, result.time%100)
-                
+                result.time = "%d:%d:%d" % (
+                    result.time // 10000, result.time // 100 % 100, result.time % 100)
+                result.amount = self.format_amount(result.amount)
+
             return render_template('token.html', token=token, results=results)
 
         @self.app.route('/report/<date>')
         def swap_report(date):
-            finished = case([(Result.status == int(Status.Swap_Finish), 1)], else_=0)
-            total_amount = case([(Result.status == int(Status.Swap_Finish), Result.amount)], else_=0)
-            pending = case([(Result.status != int(Status.Swap_Finish), 1)], else_=0)
-            total_pending = case([(Result.status != int(Status.Swap_Finish), Result.amount)], else_=0)
+            finished = case(
+                [(Result.status == int(Status.Swap_Finish), 1)], else_=0)
+            total_amount = case(
+                [(Result.status == int(Status.Swap_Finish), Result.amount)], else_=0)
+            pending = case(
+                [(Result.status != int(Status.Swap_Finish), 1)], else_=0)
+            total_pending = case(
+                [(Result.status != int(Status.Swap_Finish), Result.amount)], else_=0)
 
             results = db.session.query(
                 Result.coin,
                 Result.token,
                 func.sum(total_amount),
-                func.sum(finished),\
-                func.sum(pending),\
-                func.sum(total_pending)).\
+                func.sum(finished),
+                func.sum(total_pending),
+                func.sum(pending)).\
                 group_by(Result.coin, Result.token, Result.date).\
                 having(Result.date == date).all()
-            
+
+            results = [(x[0], x[1], self.format_amount(x[2]),
+                        x[3], self.format_amount(x[4]), x[5]) for x in results]
             return render_template('report.html', date=date, reports=results)
 
         @self.app.route('/tx/<tx_from>')
@@ -151,8 +160,9 @@ class MainService(IService):
             for result in results:
                 result.confirm_status = constants.ConfirmStr[
                     result.confirm_status]
-                result.time = "%d:%d:%d" % (result.time//10000, result.time//100%100, result.time%100)
-
+                result.time = "%d:%d:%d" % (
+                    result.time // 10000, result.time // 100 % 100, result.time % 100)
+                result.amount = self.format_amount(result.amount)
                 result.status = constants.StatusStr[result.status]
 
             return render_template('transaction.html', tx_from=tx_from,  results=results)
@@ -170,9 +180,10 @@ class MainService(IService):
                 record['tx_from'] = r.tx_from
                 record['from'] = r.from_address
                 record['to'] = r.to_address
-                record['amount'] = r.amount
+                record['amount'] = self.format_amount(r.amount)
                 record['date'] = r.date
-                record['time'] = "%d:%d:%d" % (r.time//10000, r.time//100%100, r.time%100)
+                record['time'] = "%d:%d:%d" % (
+                    r.time // 10000, r.time // 100 % 100, r.time % 100)
                 record['tx_height'] = r.tx_height
                 record['message'] = constants.ProcessStr(
                     r.status, r.confirm_status)
@@ -203,3 +214,13 @@ class MainService(IService):
 
         if hasattr(self, 'swap'):
             self.swap.stop()
+
+    def format_amount(self, amount):
+        amount_str = str(amount)
+        if amount_str.find('.') != -1:
+            amount_str = amount_str.rstrip('0')
+            if amount_str.endswith('.'):
+                amount_str = amount_str[0:len(amount_str) - 1]
+        if amount_str == '0E-18':
+            amount_str = '0'
+        return amount_str
