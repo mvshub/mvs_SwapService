873d235b8201c12d2d7d2cf5716c518658d7a792
luozhaohui <luozhaohui@viewfin.com>
2018-10-26 19:21:32 +0800

    update rate.

--- utils/exchange_rate.py
+++ utils/exchange_rate.py
@@ -4,13 +4,20 @@ from utils.exception import RpcException, CriticalException, RpcErrorException
 from utils.decimal_encoder import DecimalEncoder
 from utils import date_time
 import json
+import datetime
 from models.constants import Status, Error, SwapException
 from models import constants
 
 
 class ExchangeRate:
-    coinmarketcap_etpeth_url = 'https://api.coinmarketcap.com/v2/ticker/1703/?convert=ETH'
-    rightbtc__etpeth_url = 'https://www.rightbtc.com/api/public/ticker/ETPETH'
+    bitfinex_ethusd_url = 'https://api.bitfinex.com/v1/pubticker/ethusd'
+    bitfinex_etpusd_url = 'https://api.bitfinex.com/v1/pubticker/etpusd'
+
+    rightbtc_ethusd_url = 'https://www.rightbtc.com/api/public/ticker/ETHUSD'
+    rightbtc_etpusd_url = 'https://www.rightbtc.com/api/public/ticker/ETPUSD'
+
+    # coinmarketcap_etpeth_url = 'https://api.coinmarketcap.com/v2/ticker/1703/?convert=ETH'
+    # rightbtc__etpeth_url = 'https://www.rightbtc.com/api/public/ticker/ETPETH'
 
     @classmethod
     def request_url(cls, url):
@@ -27,19 +34,19 @@ class ExchangeRate:
             url = ExchangeRate.coinmarketcap_etpeth_url
             js = ExchangeRate.request_url(url)
             if not isinstance(js, dict) or js.get('data') is None:
-                raise RpcErrorException('not data item.')
+                raise RpcErrorException('no data item.')
 
             data = js['data']
             if not isinstance(data, dict) or data.get('quotes') is None:
-                raise RpcErrorException('not quotes item')
+                raise RpcErrorException('no quotes item')
 
             quotes = data['quotes']
             if not isinstance(quotes, dict) or quotes.get('ETH') is None:
-                raise RpcErrorException('not ETH item')
+                raise RpcErrorException('no ETH item')
 
             eth = quotes['ETH']
             if not isinstance(eth, dict) or eth.get('price') is None:
-                raise RpcErrorException('not price item')
+                raise RpcErrorException('no price item')
             value = eth.get('price')
             return 1 / value
 
@@ -49,19 +56,18 @@ class ExchangeRate:
         return None
 
     @classmethod
-    def request_rightbtc_rate(cls):
+    def request_rightbtc_rate(cls, url):
         try:
-            url = ExchangeRate.rightbtc__etpeth_url
             js = ExchangeRate.request_url(url)
             if not isinstance(js, dict) or js.get('result') is None:
-                raise RpcErrorException('not data item.')
+                raise RpcErrorException('no data item.')
 
             result = js['result']
-            if not isinstance(result, dict) or result.get('sell') is None:
-                raise RpcErrorException('not sell item')
+            if not isinstance(result, dict) or result.get('last') is None:
+                raise RpcErrorException('no last item')
 
             value = result.get('last') * (1e-8)
-            return 1 / value
+            return value
 
         except Exception as e:
             raise SwapException(Error.EXCEPTION_GET_EXCHANGE_RATE_FAIL,
@@ -69,7 +75,7 @@ class ExchangeRate:
         return None
 
     @classmethod
-    def get_etpeth_exchange_rate(cls):
+    def get_etpeth_exchange_rate_prev(cls):
         coinmarketcap_rate = ExchangeRate.request_coinmarketcap_rate()
         #connection timeout to rightbtc
         #rightbtc_rate = ExchangeRate.request_rightbtc_rate()
@@ -86,6 +92,80 @@ class ExchangeRate:
                                 'coinmarketcap_rate: {}, rightbtc_rate: {}'.format(
                                     coinmarketcap_rate, rightbtc_rate))
         ret_rate = (coinmarketcap_rate + rightbtc_rate) / 2
+
+        curr_datetime = date_time.get_beijing_datetime()
+        end_of_all_saints_day = datetime.datetime(2018, 11, 2)
+        if curr_datetime > end_of_all_saints_day:
+            ret_rate *= 0.9
+
+        return ret_rate
+
+    @classmethod 
+    def request_bitfinex_rate(cls, url):
+        try:
+            js = ExchangeRate.request_url(url)
+            if not isinstance(js, dict) or js.get('last_price') is None:
+                raise RpcErrorException('no last_price item.')
+
+            return float(js.get('last_price'))
+
+        except Exception as e:
+            raise SwapException(Error.EXCEPTION_GET_EXCHANGE_RATE_FAIL,
+                                'url: {}, error: {}'.format(url, str(e)))
+        return None
+
+    @classmethod
+    def get_bitfinex_rate(cls):
+        url = ExchangeRate.bitfinex_ethusd_url
+        ethusd_rate = ExchangeRate.request_bitfinex_rate(url)
+
+        url = ExchangeRate.bitfinex_etpusd_url
+        etpusd_rate = ExchangeRate.request_bitfinex_rate(url)
+
+        if not ethusd_rate or not etpusd_rate:
+            return None
+
+        return ethusd_rate/etpusd_rate
+
+    @classmethod
+    def get_rightbtc_rate(cls):
+        url = ExchangeRate.rightbtc_ethusd_url
+        ethusd_rate = ExchangeRate.request_rightbtc_rate(url)
+
+        url = ExchangeRate.rightbtc_etpusd_url
+        etpusd_rate = ExchangeRate.request_rightbtc_rate(url)
+
+        if not ethusd_rate or not etpusd_rate:
+            return None
+
+        return ethusd_rate/etpusd_rate
+
+    @classmethod
+    def get_etpeth_exchange_rate(cls):
+        bitfinex_rate = ExchangeRate.get_bitfinex_rate()
+        rightbtc_rate = ExchangeRate.get_rightbtc_rate()
+
+        if not bitfinex_rate and not rightbtc_rate:
+            raise SwapException(Error.EXCEPTION_GET_EXCHANGE_RATE_FAIL,
+                                'token: ETH, target: ETP')
+
+        if not bitfinex_rate:
+            bitfinex_rate = rightbtc_rate
+        elif not rightbtc_rate:
+            rightbtc_rate = bitfinex_rate
+
+        # Logger.get().info("bitfinex_rate: {}, rightbtc_rate: {}".format(
+        #     bitfinex_rate, rightbtc_rate))
+
+        # diff = abs(coinmarketcap_bitfinex_raterate - rightbtc_rate)
+        # max_fluctuation = 0.2
+        # if (diff / bitfinex_rate > max_fluctuation
+        #         or diff / rightbtc_rate > max_fluctuation):
+        #     raise SwapException(Error.EXCEPTION_INVAILD_EXCHANGE_RATE,
+        #                         'bitfinex_rate: {}, rightbtc_rate: {}'.format(
+        #                             bitfinex_rate, rightbtc_rate))
+        # ret_rate = (bitfinex_rate + rightbtc_rate) / 2
+        ret_rate = min(bitfinex_rate, rightbtc_rate)
 
         curr_datetime = date_time.get_beijing_datetime()
         end_of_all_saints_day = datetime.datetime(2018, 11, 2)
